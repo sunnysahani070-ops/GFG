14ec54eeca072d4fc7695c142f7fa9eef44b7409
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-06-28 17:10:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subsets/subsets.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    void subset(vector<int> &arr ,int i,int n,vector<int> &temp,vector<vector<int>> &ans){
+        if(i==n){
+            ans.push_back(temp);
+            return;
+        };
+        
+        temp.push_back(arr[i]);
+        subset(arr,i+1,n,temp,ans);
+        temp.pop_back();
+        subset(arr,i+1,n,temp,ans);
+    };
+  public:
+    vector<vector<int>> subsets(vector<int>& arr) {
+        // code here
+        vector<int> temp;
+        vector<vector<int>> ans;
+        subset(arr,0,arr.size(),temp,ans);
+        return ans;
+    };
+};
