4f7e49aa5bd136ccbd4c8365491222e4844de025
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-09-05 00:25:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Delete Head of Doubly Linked List/delete-head-of-doubly-linked-list.cpp
+++ Difficulty: Easy/Delete Head of Doubly Linked List/delete-head-of-doubly-linked-list.cpp
@@ -1,29 +1,16 @@
-/*
-struct Node
-{
-    int data;
-    Node * next;
-    Node * prev;
-    Node (int x)
-    {
-        data=x;
-        next=NULL;
-        prev=NULL;
-    }
+class Solution {
+public:
+    Node* deleteHead(Node* head) {
+        if(head == NULL || head->next == NULL){
+            return NULL;
+        }
+        Node* prev = head;
+        head = head->next;
 
-};
-*/
-Node *deleteHead(Node *head) {
-    // Your code here
-    if(head==NULL || head->next== NULL){
-        return NULL;
+        head->prev = nullptr;
+        prev->next = nullptr;
+
+        delete prev;
+        return head;
     }
-    Node*prev=head;
-    head=head->next;
-    
-    head->prev=nullptr;
-    prev->next=nullptr;
-    
-    delete prev;
-    return head;
-}
+};
